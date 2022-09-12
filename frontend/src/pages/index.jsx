817e2f28d98afa77d6c0f4b96df576654fe1caa4
Sergio Majé <smajefranco@gmail.com>
2022-09-12 09:45:04 -0500

    :art: Add home structure

--- frontend/src/pages/index.jsx
+++ frontend/src/pages/index.jsx
@@ -1,8 +1,24 @@
+import { useEffect } from 'react';
+
+import styles from '@Styles/pages/Home.module.scss';
+
 const Home = () => {
+    /** Add home class to the root element. */
+    useEffect(() => {
+        const rootElement = document.getElementById('__next');
+
+        rootElement.classList.add(styles.home);
+
+        return () => {
+            rootElement.classList.remove(styles.home)
+        };
+    }, [])
+
     return (
         <>
             <h1>Hola Medintegral IPS SAS</h1>
         </>
     )
 }
+
 export default Home;
