0e1cf039ace0e176b031a2982129db87ddfa9fe4
Sergio Majé <smajefranco@gmail.com>
2022-09-14 22:17:10 -0500

    :construction: Add routes in the routes helpers

--- frontend/src/helpers/routes.js
+++ frontend/src/helpers/routes.js
@@ -3,7 +3,11 @@ const routes = {
     appointment: '/citas',
     services: '/servicios',
     aboutUs: '/#sobre-nosotros',
-    login: '/iniciar-sesion'
+    login: '/iniciar-sesion',
+    about: '/quienes-somos',
+    bulletin: '/boletin',
+    socialParticipation: '/participacion-social',
+    ratings: '/valoraciones'
 }
 
 export default routes;
