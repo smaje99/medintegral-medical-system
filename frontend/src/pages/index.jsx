84bc74acc035df77bedf40feb5fc42840eea3ab1
Sergio Majé <smajefranco@gmail.com>
2022-09-14 22:20:07 -0500

    :sparkles: Add About us section in the home page

--- frontend/src/pages/index.jsx
+++ frontend/src/pages/index.jsx
@@ -1,9 +1,24 @@
+import Card from '@Components/Card';
 import { HomeLayout } from '@Components/layouts';
+
+import routes from '@Helpers/routes';
+
+import styles from '@Styles/pages/Home.module.scss';
+
+
 
 const Home = () => {
     return (
         <>
-            <h1>Hola Medintegral IPS SAS</h1>
+            <section className={styles.about_us}>
+                <span id="sobre-nosotros" style={{visibility: 'none', height: '4rem'}}></span>
+                <h2 className={styles.title}>Sobre nosotros</h2>
+                <section className={styles.group}>
+                    {items.map((item) => (
+                        <Card {...item} />
+                    ))}
+                </section>
+            </section>
         </>
     )
 }
