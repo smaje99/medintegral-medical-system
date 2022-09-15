1758620d506e99e9b0eb1af789034dcd4383041b
Sergio Majé <smajefranco@gmail.com>
2022-09-15 01:17:29 -0500

    :art: Add input button format
    Add input button and re-structuring Button component

--- frontend/src/components/Button/Button.jsx
+++ frontend/src/components/Button/Button.jsx
@@ -10,6 +10,10 @@ const ButtonAnchor = ({href, children, ...props }) => (
     </Link>
 )
 
+const ButtonInput = (props) => (
+    <input {...props} />
+)
+
 const ButtonAux = ({ children, ...props }) => (
     <button {...props}>
         {children}
@@ -17,27 +21,39 @@ const ButtonAux = ({ children, ...props }) => (
 )
 
 const Button = ({ href, className, style, as="a", children, ...props }) => {
-    const isAnchor = () => as === 'a';
+    const getButton = () => {
+        switch (as) {
+            case 'a':
+                return (
+                    <ButtonAnchor
+                        href={href}
+                        className={`${styles.button} ${styles[style]} ${className}`}
+                        {...props}
+                    >
+                        {children}
+                    </ButtonAnchor>
+                )
+            case 'button':
+                return (
+                    <ButtonAux
+                        className={`${styles.button} ${styles[style]} ${className}`}
+                        {...props}
+                    >
+                        {children}
+                    </ButtonAux>
+                )
+            case 'input':
+                return (
+                    <ButtonInput
+                        className={`${styles.button} ${styles[style]} ${className}`}
+                        value={children}
+                        {...props}
+                    />
+                )
+        }
+    }
 
-    return (
-        <>
-            {isAnchor()
-                ? (<ButtonAnchor
-                    href={href}
-                    className={`${styles.button} ${styles[style]} ${className}`}
-                    {...props}
-                >
-                    {children}
-                </ButtonAnchor>)
-                : (<ButtonAux
-                    className={`${styles.button} ${styles[style]} ${className}`}
-                    {...props}
-                >
-                    {children}
-                </ButtonAux>)
-            }
-        </>
-    )
+    return <>{getButton()}</>;
 }
 
 export default Button;
