7225fea5755e6ec5f7c4fe28d14e5b24ebfd8e87
Sergio Majé <smajefranco@gmail.com>
2022-09-14 22:21:01 -0500

    :poop: Add items for about us cards

--- frontend/src/pages/index.jsx
+++ frontend/src/pages/index.jsx
@@ -5,7 +5,38 @@ import routes from '@Helpers/routes';
 
 import styles from '@Styles/pages/Home.module.scss';
 
-
+const items = [
+    {
+        title: 'Quiénes Somos',
+        content: 'Conoce sobre nosotros y nuestra labor',
+        route: routes.about
+    },
+    {
+        title: 'Servicios',
+        content: 'Conoce nuestro portafolio de servicios médicos',
+        route: routes.services
+    },
+    {
+        title: 'Boletín',
+        content: 'Infórmate con nuestras publicaciones',
+        route: routes.bulletin
+    },
+    {
+        title: 'Participación social',
+        content: 'Conoce nuestro participación social',
+        route: routes.socialParticipation
+    },
+    {
+        title: 'Citas',
+        content: 'Agenda y consulta tus cita médicas',
+        route: routes.appointment
+    },
+    {
+        title: 'Valoraciones',
+        content: 'Para nosotros es importante saber que piensas de nosotros',
+        route: routes.ratings
+    }
+]
 
 const Home = () => {
     return (
