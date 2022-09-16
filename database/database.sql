6fac3af3a1a0efc4c475c63f87926f2d70433a16
Sergio Majé <smajefranco@gmail.com>
2022-09-16 00:35:44 -0500

    :construction: Add suggestion.suggestion table

--- database/database.sql
+++ database/database.sql
@@ -4,3 +4,20 @@ create database medintegral_medical_system;
 
 -- configurations
 create extension if not exists "uuid-ossp";
+
+
+-- schemas
+create schema if not exists suggestion;
+
+
+-- tables, constraints, indexes, views, stored procedures and triggers
+create table if not exists suggestion.suggestion (
+    id uuid not null default uuid_generate_v4(),
+    opinion varchar(500) not null,
+    pinned boolean not null default false,
+    created_at timestamp not null default now(),
+
+    constraint pk_rating primary key (id)
+);
+
+create index idx_suggestion on suggestion.suggestion (pinned);
