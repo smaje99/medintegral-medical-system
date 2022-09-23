c5000c225ec3dc8bbee7a514587219bb18ca1327
Sergio Majé <smajefranco@gmail.com>
2022-09-23 09:50:56 -0500

    :mag: Add document meta tags

--- frontend/src/pages/_document.jsx
+++ frontend/src/pages/_document.jsx
@@ -4,6 +4,12 @@ export default function Document() {
     return (
         <Html lang="es">
             <Head>
+                <meta charset="utf-8" />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
+                <meta name="description" content="Sistema médico para Medicina Integral del Caquetá I.P.S. S.A.S." />
+                <meta name="author" content="Sergio Majé" />
+                <meta name="copyright" content="Medicina Integral del Caquetá I.P.S. S.A.S." />
+                <meta name="robots" content="index, follow" />
             </Head>
             <body>
                 <Main />
