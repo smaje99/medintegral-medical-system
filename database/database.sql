31b999b1989b51b240d39bfa8687d3f5c71c476e
Sergio Majé <smajefranco@gmail.com>
2022-09-26 16:54:19 -0500

    🗃️ (database): Add trigger to control of pinned suggestions
    the trigger works for data creation and update, and only allows to have three pinned 	suggestions

--- database/database.sql
+++ database/database.sql
@@ -1,3 +1,4 @@
+-- Active: 1658088866111@@127.0.0.1@5432@medintegral_medical_system
 create database medintegral_medical_system;
 
 \c medintegral_medical_system
@@ -11,6 +12,8 @@ create schema if not exists suggestion;
 
 
 -- tables, constraints, indexes, views, stored procedures and triggers
+
+-- suggestion
 create table if not exists suggestion.suggestion (
     id uuid not null default uuid_generate_v4(),
     opinion varchar(500) not null,
@@ -21,3 +24,36 @@ create table if not exists suggestion.suggestion (
 );
 
 create index idx_suggestion on suggestion.suggestion (pinned);
+
+create or replace function suggestion.fnSuggestion_setPinned()
+    returns trigger
+    language plpgsql
+    as
+$$
+declare
+    pinned_count integer;
+begin
+    if new.pinned then
+        select distinct
+            count(pinned) into pinned_count
+        from
+            suggestion.suggestion
+        where
+            pinned = true
+        group by
+            pinned;
+
+        if pinned_count >= 3 then
+            raise exception 'Hay 3 sugerencias ancladas ya';
+        end if;
+    end if;
+
+    return new;
+end
+$$;
+
+create or replace trigger trSuggestion_setPinned
+    before insert or update
+    on suggestion.suggestion
+    for each row
+    execute procedure suggestion.fnSuggestion_setPinned();
