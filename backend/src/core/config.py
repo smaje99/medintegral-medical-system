ae328a9603fa79c004f1fed349cff04d9074b503
Sergio Majé <smajefranco@gmail.com>
2022-09-28 18:53:47 -0500

    🌱 (backend): Create FastAPI application root
    Add secrets for project specification and cors

--- backend/src/core/config.py
+++ backend/src/core/config.py
@@ -1,17 +1,30 @@
 from typing import Any
 
-from pydantic import BaseSettings, BaseConfig, PostgresDsn, validator
+from pydantic import AnyHttpUrl, BaseSettings, BaseConfig, PostgresDsn, validator
 
 
 class Settings(BaseSettings):
+    API_VERSION: str
+
+    PROJECT_NAME: str
+    PROJECT_VERSION: str
+    PROJECT_DESCRIPTION: str
+
+    BACKEND_CORS_ORIGINS: list[AnyHttpUrl | str] = ['*']
+
+    @validator('BACKEND_CORS_ORIGINS', pre=True)
+    def assemble_cors_origins(cls, v: str | list[str]) -> list[str] | str:
+        if isinstance(v, str) and not v.startswith('['):
+            return [i.strip() for i in v.split(',')]
+        elif isinstance(v, (list, str)):  # pyright: ignore
+            return v
+        raise ValueError(v)
+
     POSTGRES_HOST: str
     POSTGRES_PORT: str
     POSTGRES_UID: str
     POSTGRES_PWD: str
     POSTGRES_DB: str
-
-    DATABASE_ECHO: bool
-
     SQLALCHEMY_DATABASE_URI: PostgresDsn | None = None
 
     @validator('SQLALCHEMY_DATABASE_URI', pre=True)
@@ -27,6 +40,8 @@ class Settings(BaseSettings):
             port=values.get('POSTGRES_PORT'),
             path=f"/{values.get('POSTGRES_DB') or ''}"
         )
+
+    DATABASE_ECHO: bool
 
     class Config(BaseConfig):  # pyright: ignore
         env_file = '.env'
--- backend/src/main.py
+++ backend/src/main.py
@@ -1,5 +1,32 @@
+from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
+import uvicorn  # pyright: ignore
+
 from core.config import settings
+from database.event import init_db
+
+
+app = FastAPI(
+    title=settings.PROJECT_NAME,
+    description=settings.PROJECT_DESCRIPTION,
+    version=settings.PROJECT_VERSION,
+    openapi_url=f'{settings.API_VERSION}/openapi.json'
+)
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[str(origin) for origin in settings.BACKEND_CORS_ORIGINS],
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*']
+)
+
+
+@app.on_event('startup')  # pyright: ignore
+async def startup():
+    await init_db()
 
 
 if __name__ == '__main__':
-    print(settings.SQLALCHEMY_DATABASE_URI)
+    uvicorn.run('main:app')  # pyright: ignore
