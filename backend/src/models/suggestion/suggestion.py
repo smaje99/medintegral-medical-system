8f3ac9888cd1f56468074b2d43a5f1ec31b0844e
Sergio Majé <smajefranco@gmail.com>
2022-09-29 19:51:15 -0500

    🚧 (backend): Add suggestion.suggestion table

---
+++ backend/src/models/suggestion/suggestion.py
@@ -0,0 +1,18 @@
+from sqlalchemy import Boolean, Column, String
+from sqlalchemy.dialects.postgresql import UUID, TIMESTAMP
+from sqlalchemy.sql import expression, func
+
+from database import Base  # pyright: ignore
+
+class Suggestion(Base):  # pyright: ignore
+    id = Column(
+        UUID(as_uuid=True),
+        primary_key=True,
+        nullable=False,
+        server_default=func.uuid_generate_v4()
+    )
+    opinion = Column(String(500))
+    pinned = Column(Boolean, server_default=expression.false(), index=True)
+    create_at = Column(TIMESTAMP, server_default=func.current_timestamp())
+
+    __table_args__ = { 'schema': 'suggestion' }
