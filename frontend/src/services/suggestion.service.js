9914e91e5327010587dcc9cd6f7081bf974a8779
Sergio Majé <smajefranco@gmail.com>
2022-10-03 15:02:15 -0500

    👔 (frontend): Create Suggestion services

---
+++ frontend/src/services/suggestion.service.js
@@ -0,0 +1,9 @@
+import axios from 'axios';
+
+const { NEXT_PUBLIC_API } = process.env;
+const baseURL = `${NEXT_PUBLIC_API}/suggestion/`;
+
+export const createSuggestion = async ({ opinion }) => {
+    const { data } = await axios.post(baseURL, { opinion });
+    return data;
+}
