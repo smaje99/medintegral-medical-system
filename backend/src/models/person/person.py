42be34dac2c4713bc789a560cf134c65143c8960
Sergio Majé <smajefranco@gmail.com>
2022-10-10 10:26:53 -0500

    🤡 (backend): Add not null constraint to Person columns
    Add not null constraint to the create date and modification date of the person model

--- backend/src/models/person/person.py
+++ backend/src/models/person/person.py
@@ -12,7 +12,7 @@ class Person(Base):
     name = Column(Text, nullable=False)
     surname = Column(Text, nullable=False)
     address = Column(Text)
-    email = Column(EmailType, nullable=False)  # pyright: ignore
+    email = Column(EmailType, nullable=False, unique=True)  # pyright: ignore
     phone = Column(PhoneNumberType(), nullable=False)  # pyright: ignore
     gender = Column(
         Enum(Gender, values_callable=lambda obj: [e.value for e in obj]),  # pyright: ignore
@@ -32,9 +32,14 @@ class Person(Base):
     civil_status = Column(
         Enum(CivilStatus, values_callable=lambda obj: [e.value for e in obj])  # pyright: ignore
     )
-    created_at = Column(TIMESTAMP(timezone=True), server_default=func.current_timestamp())
+    created_at = Column(
+        TIMESTAMP(timezone=True),
+        nullable=False,
+        server_default=func.current_timestamp()
+    )
     modified_at = Column(
         TIMESTAMP(timezone=True),
+        nullable=False,
         server_default=func.current_timestamp(),
         onupdate=func.current_timestamp()
     )
