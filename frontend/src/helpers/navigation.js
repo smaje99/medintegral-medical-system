79629677c1b1e5441c08a0a637fb27423fae1dcf
Sergio Majé <smajefranco@gmail.com>
2022-10-11 17:57:49 -0500

    💩 (frontend): Add navigation items in helpers

---
+++ frontend/src/helpers/navigation.js
@@ -0,0 +1,9 @@
+import routes from '@Helpers/routes';
+
+const navigation = [
+    { name: 'Servicios', route: routes.services },
+    { name: 'Citas', route: routes.appointment },
+    { name: 'Sobre nosotros', route: routes.aboutUs }
+]
+
+export default navigation;
