81b45d3af0e8c5a61657f66112e50ddb8eb08cb9
Sergio Majé <smajefranco@gmail.com>
2022-10-29 00:40:01 -0500

    🚚 (backend): Rename suggestion schema

--- backend/app/schemas/suggestion/__init__.py
+++
@@ -1,6 +0,0 @@
-from .suggestion_schema import (
-    Suggestion,  # pyright: ignore
-    SuggestionCreate,  # pyright: ignore
-    SuggestionUpdate,  # pyright: ignore
-    SuggestionInDB  # pyright: ignore
-)
