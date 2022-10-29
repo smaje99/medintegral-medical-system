e34cdd1eccd82d7dae22d8a9b9d54d1c51ac8a05
Sergio Majé <smajefranco@gmail.com>
2022-10-29 17:46:06 -0500

    💄 (frontend): Add last details of the login form

--- frontend/src/components/Forms/LoginForm/LoginForm.jsx
+++ frontend/src/components/Forms/LoginForm/LoginForm.jsx
@@ -1,5 +1,11 @@
-import { forwardRef, useEffect, useImperativeHandle } from 'react';
+import {
+    forwardRef,
+    useEffect,
+    useImperativeHandle,
+    useState
+} from 'react';
 import { useForm } from 'react-hook-form';
+import { BsEyeFill, BsEyeSlashFill } from 'react-icons/bs';
 
 import Button from '@Components/Button';
 
@@ -7,6 +13,12 @@ import styles from './LoginForm.module.scss';
 
 const LoginForm = forwardRef((props, ref) => {
     const { handleSubmit, register, reset } = useForm();
+    const [showPassword, setShowPassword] = useState(false);
+
+    const handleShowPassword = (e) => {
+        e.preventDefault();
+        setShowPassword(!showPassword);
+    }
 
     const handleLogin = async (formData) => {}
 
@@ -21,23 +33,44 @@ const LoginForm = forwardRef((props, ref) => {
             onSubmit={handleSubmit(handleLogin)}
             autoComplete="on"
         >
-            <input
-                type="text"
-                id="username"
-                className={styles.input}
-                placeholder="Usuario"
-                autoFocus={true}
-                required
-                {...register('username')}
-            />
-            <input
-                type="password"
-                id="password"
-                className={styles.input}
-                placeholder="Contraseña"
-                required
-                {...register('password')}
-            />
+            <div className={styles.field}>
+                <input
+                    type="text"
+                    id="username"
+                    className={styles.input}
+                    placeholder="Usuario"
+                    autoFocus={true}
+                    required
+                    {...register('username')}
+                />
+            </div>
+            <div className={styles.field}>
+                <input
+                    type={showPassword ? 'text': 'password'}
+                    id="password"
+                    className={styles.input}
+                    placeholder="Contraseña"
+                    required
+                    {...register('password')}
+                />
+                <button
+                    className={styles.show_password}
+                    onClick={handleShowPassword}
+                >
+                    {showPassword ? <BsEyeFill /> : <BsEyeSlashFill />}
+                </button>
+            </div>
+            <label htmlFor="remember-me" className={styles.container}>
+                <input
+                    type="checkbox"
+                    id="remember-me"
+                    className={styles.checkbox}
+                    {...register('rememberMe')}
+                />
+                <span className={styles.text}>
+                    Mantener la sesión iniciada
+                </span>
+            </label>
 
             <Button
                 type="submit"
