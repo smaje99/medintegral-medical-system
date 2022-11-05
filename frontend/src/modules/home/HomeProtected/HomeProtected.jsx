216f7b7a6ce279c7ea20213fcbef8adf807c4082
Sergio Majé <smajefranco@gmail.com>
2022-11-05 01:59:58 -0500

    🚧 (frontend): Add Home protected page elements

--- frontend/src/modules/home/HomeProtected/HomeProtected.jsx
+++ frontend/src/modules/home/HomeProtected/HomeProtected.jsx
@@ -1,8 +1,24 @@
+import Image from 'next/future/image';
+
+import { ProtectedLayout } from '@Components/layouts';
+
 import styles from './HomeProtected.module.scss';
+
+import medintegralIcon from '@Icons/medintegral.svg';
 
 const HomeProtected = () => {
     return (
-        <div>HomeProtected</div>
+        <ProtectedLayout title="Inicio">
+            <main className={styles.home}>
+                <Image
+                    src={medintegralIcon}
+                    className={styles.brand}
+                    alt="Medintegral I.P.S. S.A.S."
+                    priority
+                />
+                <span className={styles.platform}>Plataforma médica</span>
+            </main>
+        </ProtectedLayout>
     )
 }
 
