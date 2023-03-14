c86d01cb1c42d1cf3acfff4603280720ae25aebc
Sergio Majé <smajefranco@gmail.com>
2023-03-13 23:45:18 -0500

    🚸 (frontend): Inform the user of the copied content

--- frontend/src/components/Button/CopyButton/CopyButton.tsx
+++ frontend/src/components/Button/CopyButton/CopyButton.tsx
@@ -1,11 +1,15 @@
 import { IoCopy } from 'react-icons/io5';
+import { toast } from 'react-toastify';
+
+import getToastConfig from '@Helpers/toast.config';
 
 import Button from '../Button';
 import type { CopyButtonProps } from './CopyButton.types';
 
 const CopyButton: React.FC<CopyButtonProps> = ({ textToCopy }) => {
-    const handleCopy = () => {
-        navigator.clipboard.writeText(textToCopy);
+    const handleCopy = async () => {
+        await navigator.clipboard.writeText(textToCopy);
+        toast.info('Contenido copiado', getToastConfig())
     }
 
     return (
