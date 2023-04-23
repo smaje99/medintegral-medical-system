351c1942a3eb3fd35403b07db27defe0727bd470
Sergio Majé <smajefranco@gmail.com>
2023-04-23 01:39:04 -0500

    :wrench: (frontend): Add remotePatterns to images configuration
    The remotePatterns configuration is added to the images configuration object. This allows the application to load images from a remote server with the specified protocol, hostname, port, and pathname. In this case, the remote server is located at http://127.0.0.1:8000 and serves images from the /files directory.

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,5 +3,13 @@ const path = require('path');
 module.exports = {
     sassOptions: {
         includePaths: [path.join(__dirname, 'src', 'styles')]
+    },
+    images: {
+        remotePatterns: [{
+            protocol: 'http',
+            hostname: '127.0.0.1',
+            port: '8000',
+            pathname: '/files/**'
+        }]
     }
 }
