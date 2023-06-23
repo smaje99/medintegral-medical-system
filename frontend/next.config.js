c25e7351e5054ace51c40da262b0056b08890ae6
Sergio Majé <smajefranco@gmail.com>
2023-06-22 19:26:46 -0500

    🔧 (frontend): Implement ESLint and Prettier

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,15 +1,18 @@
 const path = require('path');
 
+/** @type {import('next').NextConfig} */
 module.exports = {
-    sassOptions: {
-        includePaths: [path.join(__dirname, 'src', 'styles')]
-    },
-    images: {
-        remotePatterns: [{
-            protocol: 'http',
-            hostname: '127.0.0.1',
-            port: '8000',
-            pathname: '/files/**'
-        }]
-    }
-}
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'src', 'styles')],
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: '127.0.0.1',
+        port: '8000',
+        pathname: '/files/**',
+      },
+    ],
+  },
+};
