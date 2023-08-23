4a6a42bab8768cb46cbe85bf3b0f7f6cc95097f9
Sergio Majé <smajefranco@gmail.com>
2023-08-22 22:11:05 -0500

    :mag: (frontend) Add metadata of platform web

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,20 +1,36 @@
-import './globals.css'
-import type { Metadata } from 'next'
+import type { Metadata } from 'next';
+
+import './globals.css';
 
 export const metadata: Metadata = {
   title: 'Medintegral IPS S.A.S',
-  description: 'Plataforma médica para citas médicas de'
-    + 'la MEDICINA INTEGRAL DEL CAQUETÁ IPS S.A.S',
-}
+  description:
+    'Plataforma médica para citas médicas de' +
+    'la MEDICINA INTEGRAL DEL CAQUETÁ IPS S.A.S',
+  authors: { name: 'Sergio Andrés Majé Franco', url: 'smajefranco@gmail.com' },
+  viewport: 'width=device-width, initial-scale=1',
+  keywords: [
+    'Medintegral',
+    'Medintegral IPS',
+    'IPS',
+    'medicina',
+    'citas',
+    'citas médicas',
+    'agenda médica',
+    'medicina integral',
+    'Caquetá',
+    'sugerencias'
+  ],
+  openGraph: {
+    title: 'Medintegral IPS S.A.S',
+    description: 'Plataforma médica de Medintegral IPS S.A.S',
+  },
+};
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="es">
+    <html lang='es'>
       <body>{children}</body>
     </html>
-  )
+  );
 }
