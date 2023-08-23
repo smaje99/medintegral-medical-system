fa804e0c3d177e4df52107147e73064660245f3f
Sergio Majé <smajefranco@gmail.com>
2023-08-22 21:48:12 -0500

    :wrench: Add project config files
    Set eslint, prettier, typescript, sass and next.js configuration

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'src', 'styles')],
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: '127.0.0.1',
+        port: '8000',
+        pathname: '/files/**',
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
+
