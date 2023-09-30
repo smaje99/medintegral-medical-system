0294ff0b9797bbea85ae3d3f8f3e02398ddd9519
Sergio Majé <smajefranco@gmail.com>
2023-09-30 06:10:37 -0500

    🧱(backend): Add revisions to database
    Adds UUID extension and creates the user schema

---
+++ backend/alembic/versions/2868a987ef68_create_uuid_extension.py
@@ -0,0 +1,27 @@
+"""create uuid extension
+
+Revision ID: 2868a987ef68
+Revises:
+Create Date: 2023-09-30 05:16:00.926314
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+
+
+# revision identifiers, used by Alembic.
+revision: str = '2868a987ef68'
+down_revision: Union[str, None] = None
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    '''Initialize uuid extension'''
+    op.execute('CREATE EXTENSION IF NOT EXISTS \"uuid-ossp\";')  # pylint: disable=E1101
+
+
+def downgrade() -> None:
+    '''Drop uuid extension'''
+    op.execute('DROP EXTENSION IF EXISTS \"uuid-ossp\";')  # pylint: disable=E1101
---
+++ backend/alembic/versions/f250d08aafab_create_user_schema.py
@@ -0,0 +1,27 @@
+"""create user schema
+
+Revision ID: f250d08aafab
+Revises: 2868a987ef68
+Create Date: 2023-09-30 06:00:24.931595
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+
+
+# revision identifiers, used by Alembic.
+revision: str = 'f250d08aafab'
+down_revision: Union[str, None] = '2868a987ef68'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    '''Creates the user schema'''
+    op.execute('CREATE SCHEMA IF NOT EXISTS "user";')  # pylint: disable=E1101
+
+
+def downgrade() -> None:
+    '''Drops the user schema'''
+    op.execute('DROP SCHEMA IF EXISTS "user";')  # pylint: disable=E1101
