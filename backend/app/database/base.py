422beecadf507bd1eb228c682dd9a333a32f3d3c
Sergio Majé <smajefranco@gmail.com>
2023-11-25 01:31:44 -0500

    👽️ Migrate Base class for SQLAlchemy 2.0 async
    Includes modifications to the Base class to adapt it to the changes introduced in SQLAlchemy 2.0, Additionally, the Base has been configured to support asynchronous entities. Also, __tablename__ is generating the table name via the entity, removing its prefix and sufix.

--- backend/app/database/base.py
+++ backend/app/database/base.py
@@ -1,14 +1,20 @@
-from sqlalchemy.ext.declarative import declared_attr
 from sqlalchemy.ext.asyncio import AsyncAttrs
+from sqlalchemy.ext.declarative import declared_attr
 from sqlalchemy.orm import DeclarativeBase, MappedAsDataclass
 
 
+__all__ = ('Base',)
+
+
 class Base(AsyncAttrs, MappedAsDataclass, DeclarativeBase):
-    '''Base class for all SQLAlchemy models.'''
+  '''Base class for all SQLAlchemy models.
 
-    __name__: str
+  The class models must have a name that starts with "Orm" and ends with "Model".
+  '''
 
-    @declared_attr
-    def __tablename__(self) -> str:
-        '''Generate __tablename__ automatically'''
-        return self.__name__.lower()
+  __name__: str
+
+  @declared_attr.directive
+  def __tablename__(cls) -> str:
+    '''Generate __tablename__ automatically.'''
+    return cls.__name__.removeprefix('Orm').removesuffix('Entity').lower()
