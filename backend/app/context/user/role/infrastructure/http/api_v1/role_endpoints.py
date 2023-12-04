50d3b534e2ffa817c42bb9af1cc1ae0a9fce8a61
Sergio Majé <smajefranco@gmail.com>
2023-12-04 00:30:06 -0500

    🩹(backend): Fix circular dependencies
    Fix circular dependencies between the container and its inject wirings.

--- backend/app/context/user/role/infrastructure/http/api_v1/role_endpoints.py
+++ backend/app/context/user/role/infrastructure/http/api_v1/role_endpoints.py
@@ -4,7 +4,6 @@ from dependency_injector.wiring import Provide, inject
 from fastapi import APIRouter, Body, Depends
 from starlette.status import HTTP_201_CREATED
 
-from app.containers import ApplicationContainer as Container
 from app.context.user.role.domain import Role, RoleSaveDTO
 from app.context.user.role.infrastructure.http.api_v1.controllers import (
   RoleCreateController,
@@ -23,7 +22,7 @@ async def create_user(  # noqa: D417
   role_in: Annotated[RoleSaveDTO, Body(alias='roleIn')],
   *,
   role_controller: RoleCreateController = Depends(  # noqa: B008
-    Provide[Container.role.role_create_controller]
+    Provide['role.role_create_controller']
   ),
 ) -> Role:
   '''Create a new role.
