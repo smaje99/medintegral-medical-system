9e8d26cb7bbfebd108993bb99166dc0c50cdd67b
Sergio Majé <smajefranco@gmail.com>
2023-12-08 23:48:43 -0500

    🥅(backend): Implement SQLAlchemyError handler

--- backend/app/app.py
+++ backend/app/app.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from sqlalchemy.exc import SQLAlchemyError
 
 from app.api import api_v1_router
 from app.containers import ApplicationContainer
@@ -9,6 +10,7 @@ from app.core.errors import HTTPException
 from app.middlewares.error_handlers import (
     dao_error_exception_handler,
     http_exception_handler,
+    sqlalchemy_error_exception_handler,
 )
 
 
@@ -39,5 +41,5 @@ app.add_middleware(
 app.include_router(api_v1_router, prefix=container.config.domain.api_version())
 
 app.add_exception_handler(DaoError, dao_error_exception_handler)
-
+app.add_exception_handler(SQLAlchemyError, sqlalchemy_error_exception_handler)
 app.add_exception_handler(HTTPException, http_exception_handler)
--- backend/app/middlewares/error_handlers.py
+++ backend/app/middlewares/error_handlers.py
@@ -1,12 +1,17 @@
 from fastapi import Request
 from fastapi.responses import JSONResponse
+from sqlalchemy.exc import SQLAlchemyError
 from starlette.status import HTTP_500_INTERNAL_SERVER_ERROR
 
 from app.context.shared.domain.errors import DaoError
 from app.core.errors import HTTPException
 
 
-__all__ = ('http_exception_handler', 'dao_error_exception_handler')
+__all__ = (
+  'http_exception_handler',
+  'dao_error_exception_handler',
+  'sqlalchemy_error_exception_handler',
+)
 
 
 def http_exception_handler(_: Request, exc: HTTPException) -> JSONResponse:
@@ -16,13 +21,20 @@ def http_exception_handler(_: Request, exc: HTTPException) -> JSONResponse:
   if exc.error_type:
     content['error_type'] = exc.error_type
 
-  return JSONResponse(
-    content=content,
-    status_code=exc.status_code,
-    headers=exc.headers
-  )
+  return JSONResponse(content=content, status_code=exc.status_code, headers=exc.headers)
 
 
 def dao_error_exception_handler(_: Request, exc: DaoError):
   '''Handler for the DAOError's.'''
   raise HTTPException(status_code=HTTP_500_INTERNAL_SERVER_ERROR, message=exc.message)
+
+
+def sqlalchemy_error_exception_handler(_: Request, exc: SQLAlchemyError):
+  '''Handler for the SQLAlchemy errors.'''
+  raise HTTPException(
+    status_code=HTTP_500_INTERNAL_SERVER_ERROR,
+    message=(
+      'Lo sentimos, actualmente no podemos procesar su solicitud debido'
+      + 'a problemas técnicos. Por favor, inténtelo de nuevo más tarde.'
+    ),
+  )
