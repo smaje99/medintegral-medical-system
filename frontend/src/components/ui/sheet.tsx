d886a33fec7b2dc2f237d5eb00393ce389cfb106
Sergio Majé <smajefranco@gmail.com>
2023-12-12 14:50:11 -0500

    👽️(frontend): Update SheetPortal component

--- frontend/src/components/ui/sheet.tsx
+++ frontend/src/components/ui/sheet.tsx
@@ -18,10 +18,7 @@ const SheetTrigger = SheetPrimitive.Trigger;
 
 const SheetClose = SheetPrimitive.Close;
 
-const SheetPortal = ({ className, ...props }: SheetPrimitive.DialogPortalProps) => (
-  <SheetPrimitive.Portal className={cn(className)} {...props} />
-);
-SheetPortal.displayName = SheetPrimitive.Portal.displayName;
+const SheetPortal = SheetPrimitive.Portal;
 
 const SheetOverlay = forwardRef<
   ElementRef<typeof SheetPrimitive.Overlay>,
