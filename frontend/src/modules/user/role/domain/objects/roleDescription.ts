d151a53fe4ec6699992400c9ef366b069ebc4db9
Sergio Majé <smajefranco@gmail.com>
2024-01-19 23:29:56 -0500

    🦺(frontend): Missing validations in role name and description

--- frontend/src/modules/user/role/domain/objects/roleDescription.ts
+++ frontend/src/modules/user/role/domain/objects/roleDescription.ts
@@ -1,3 +1,6 @@
 import { z } from 'zod';
 
-export const roleDescriptionSchema = z.string();
+export const roleDescriptionSchema = z.string({
+  required_error: 'Nombre del rol es requerido',
+  invalid_type_error: 'Nombre del rol debe ser una cadena de texto',
+});
--- frontend/src/modules/user/role/domain/objects/roleName.ts
+++ frontend/src/modules/user/role/domain/objects/roleName.ts
@@ -1,7 +1,10 @@
 import { z } from 'zod';
 
 export const roleNameSchema = z
-  .string()
+  .string({
+    required_error: 'Nombre del rol es requerido',
+    invalid_type_error: 'Nombre del rol debe ser una cadena de texto',
+  })
   .min(4, 'Nombre del rol debe tener al menos 4 caracteres')
   .max(20, 'Nombre del rol debe tener menos de 20 caracteres')
   .regex(/^[a-zA-Z]+$/, 'Nombre del rol debe contener solo letras')
