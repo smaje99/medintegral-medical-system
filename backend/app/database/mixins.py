382a7aa94bc643abd4d8d08a00e4b9421b7ee273
Sergio Majé <smajefranco@gmail.com>
2024-02-01 12:01:31 -0500

    🌱(backend): Add TimestampMixin and IsActiveMixin

--- backend/app/database/mixins.py
+++ backend/app/database/mixins.py
@@ -1,11 +1,13 @@
+from datetime import datetime
 from uuid import UUID
 
 from sqlalchemy.orm import Mapped, MappedAsDataclass, mapped_column
 from sqlalchemy.sql import func
-from sqlalchemy.types import Uuid
+from sqlalchemy.sql.expression import true
+from sqlalchemy.types import TIMESTAMP, Boolean, Uuid
 
 
-__all__ = ('IdentifierMixin',)
+__all__ = ('IdentifierMixin', 'TimestampMixin', 'IsActiveMixin')
 
 
 class IdentifierMixin(MappedAsDataclass, init=False):
@@ -18,3 +20,33 @@ class IdentifierMixin(MappedAsDataclass, init=False):
     server_default=func.uuid_generate_v4(),
   )
   '''Unique identifier of the instance.'''
+
+
+class TimestampMixin(MappedAsDataclass, init=False):
+  '''Common mixin for entities with a timestamp.'''
+
+  created_at: Mapped[datetime] = mapped_column(
+    TIMESTAMP(timezone=True),
+    nullable=False,
+    server_default=func.current_timestamp(),
+  )
+  '''Timestamp of the creation of the instance.'''
+
+  updated_at: Mapped[datetime] = mapped_column(
+    TIMESTAMP(timezone=True),
+    nullable=False,
+    server_default=func.current_timestamp(),
+    onupdate=func.current_timestamp(),
+  )
+  '''Timestamp of the last update of the instance.'''
+
+
+class IsActiveMixin(MappedAsDataclass, init=False):
+  '''Common mixin for entities with an active status.'''
+
+  is_active: Mapped[bool] = mapped_column(
+    Boolean,
+    nullable=False,
+    server_default=true(),
+  )
+  '''Status of the instance.'''
