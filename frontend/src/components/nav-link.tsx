9a44a4f75aa7a0cacf11e1700c60b6f5338b03aa
Sergio Majé <smajefranco@gmail.com>
2024-02-09 23:47:05 -0500

    🎨(frontend): Update NavLink props

--- frontend/src/components/nav-link.tsx
+++ frontend/src/components/nav-link.tsx
@@ -2,15 +2,16 @@
 
 import Link, { type LinkProps } from 'next/link';
 import { usePathname } from 'next/navigation';
-import { type ReactNode, useMemo } from 'react';
+import { type AnchorHTMLAttributes, type ReactNode, useMemo } from 'react';
 
 import { cn } from '@/lib/utils';
 
-type Props = LinkProps & {
-  className?: string;
-  activeClassName?: string;
-  children: ReactNode;
-};
+type Props = Omit<AnchorHTMLAttributes<HTMLAnchorElement>, keyof LinkProps> &
+  LinkProps & {
+    className?: string;
+    activeClassName?: string;
+    children: ReactNode;
+  };
 
 export const NavLink: React.FC<Props> = ({
   className,
