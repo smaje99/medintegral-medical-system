57543329a6909afb340f555417d814373fceb3c8
Sergio Majé <smajefranco@gmail.com>
2024-02-23 13:25:05 -0500

    🔧(frontend): Set prettier plugin tailwindcss

--- frontend/prettier.config.js
+++ frontend/prettier.config.js
@@ -2,4 +2,7 @@ module.exports = {
   printWidth: 90,
   singleQuote: true,
   jsxSingleQuote: true,
+  plugins: [require('prettier-plugin-tailwindcss')],
+  tailwindConfig: './tailwind.config.js',
+  tailwindFunctions: ['clsx', 'twMerge', 'cn']
 };
