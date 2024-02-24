16bc0f2fb35fc737a62ac765819dad9fd1e300a1
Sergio Majé <smajefranco@gmail.com>
2024-02-24 12:00:00 -0500

    🔧(frontend): Set config files

--- frontend/prettier.config.js
+++ frontend/prettier.config.js
@@ -1,8 +1,11 @@
-module.exports = {
+/** @type {import("prettier").Config} */
+const config = {
   printWidth: 90,
   singleQuote: true,
   jsxSingleQuote: true,
-  plugins: [require('prettier-plugin-tailwindcss')],
-  tailwindConfig: './tailwind.config.js',
-  tailwindFunctions: ['clsx', 'twMerge', 'cn']
+  // plugins: ['prettier-plugin-tailwindcss'],
+  // tailwindConfig: './tailwind.config.js',
+  // tailwindFunctions: ['clsx', 'twMerge', 'cn']
 };
+
+export default config;
