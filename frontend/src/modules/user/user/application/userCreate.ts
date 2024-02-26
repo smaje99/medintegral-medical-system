24151aadb009837b0438e9946a98bad83f7b8dde
Sergio Majé <smajefranco@gmail.com>
2024-02-26 17:08:08 -0500

    🧐(frontend): Manage use case data Create User

--- frontend/src/modules/user/user/application/userCreate.ts
+++ frontend/src/modules/user/user/application/userCreate.ts
@@ -1,15 +1,34 @@
+import { PersonSaveValues } from '@/modules/person/domain';
+
 import type {
   PersonAssociatedWithUserSaveValues,
   UserPostResponse,
   UserRepository,
+  UserSaveValues,
 } from '../domain';
 
 export class UserCreate {
   constructor(private readonly repository: UserRepository) {}
 
-  async run(
-    userIn: PersonAssociatedWithUserSaveValues,
-  ): Promise<UserPostResponse | undefined> {
-    return this.repository.save(userIn);
+  async run({
+    dni,
+    bloodType: BTWithRhFactor,
+    roleId,
+    image,
+    ...restPerson
+  }: PersonAssociatedWithUserSaveValues): Promise<UserPostResponse | undefined> {
+    const splitBTWithRhFactor = BTWithRhFactor?.split(/\b/);
+    const bloodType = splitBTWithRhFactor?.[0] as PersonSaveValues['bloodType'];
+    const rhFactor = splitBTWithRhFactor?.[1] as PersonSaveValues['rhFactor'];
+
+    const user: UserSaveValues = { id: dni, roleId, image };
+    const person: PersonSaveValues = {
+      dni,
+      bloodType,
+      rhFactor,
+      ...restPerson,
+    };
+
+    return this.repository.save(user, person);
   }
 }
